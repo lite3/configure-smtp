b725e7c6872f21aa9bfd8d6ed30c2a2eb0edc398
zheyu <lite3@qq.com>
2017-02-25 18:06:23 +0800

    fix some warning

--- configure-smtp.php
+++ configure-smtp.php
@@ -62,15 +62,11 @@ class c2c_ConfigureSMTP extends C2C_Plugin_023 {
 	 * @return void
 	 */
 	public function __construct() {
-		$this->c2c_ConfigureSMTP();
-	}
-
-	public function c2c_ConfigureSMTP() {
 		// Be a singleton
 		if ( ! is_null( self::$instance ) )
 			return;
 
-		$this->C2C_Plugin_023( '3.1', 'configure-smtp', 'c2c', __FILE__, array() );
+		parent::__construct( '3.1', 'configure-smtp', 'c2c', __FILE__, array() );
 		register_activation_hook( __FILE__, array( __CLASS__, 'activation' ) );
 		self::$instance = $this;
 	}
@@ -168,7 +164,7 @@ class c2c_ConfigureSMTP extends C2C_Plugin_023 {
 	 *
 	 * @return void (Text will be echoed.)
 	 */
-	public function options_page_description() {
+	public function options_page_description( $localized_heading_text = '' ) {
 		$options = $this->get_options();
 		parent::options_page_description( __( 'Configure SMTP Settings', $this->textdomain ) );
 		if ( ! empty( $this->error_msg ) )
